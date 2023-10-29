509c8643b66fcaa5bfb46a9a011ef501bbe15136
KennedyRyan-code <kenryan49@gmail.com>
2023-10-29 13:40:20 +0300

    error page

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Flask Application"""
-from flask import Flask
+from flask import Flask, render_template, make_response, jsonify
 from api.v1.views import app_views
 from models import storage
 
@@ -9,9 +9,20 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 
 @app.teardown_appcontext
-def close_db(exception):
+def close_db(error):
     """Closes the database on teardown."""
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """ 404 Error
+    ---
+    responses:
+      404:
+        description: resource was not found
+    """
+    return make_response(jsonify({'error': "Not found"}), 404)
 
 if __name__ == "__main__":
     host = os.getenv('HBNB_API_HOST', '0.0.0.0')
